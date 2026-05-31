79f463fd950f9067fbcbc50c89f1d7602f6cb9bd
Min_Young Park <minyoungpark5321@gmail.com>
2026-05-30 22:53:15 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = [];
+        hm = {};
+        pos = 0;
+        sumup = 0;
+        for word in strs:
+            sorted_string = ''.join(sorted(word));
+            if sorted_string not in hm.keys():
+                hm[sorted_string] = pos;
+                pos += 1;
+                res.append([word]);
+            else:
+                res[hm[sorted_string]].append(word);
+            sumup = 0;
+        print(hm);
+        return res;
