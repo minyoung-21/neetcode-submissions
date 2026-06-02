2ecee3121c0ad44a4662e73e41afda1044cb4de6
Min_Young Park <minyoungpark5321@gmail.com>
2026-06-01 21:37:13 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,49 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        hm_row = defaultdict(set);
+        hm_col = defaultdict(set);
+        hm_box = defaultdict(set);
+
+        col_count = 0;
+        repeated = False;
+
+        for i in range(9):
+            for j in range(9):
+                #check row
+                if board[j][i] in hm_col[i]:
+                    return False;
+                if board[i][j] in hm_row[i]:
+                    return False;
+                if board[i][j] in hm_box[(i // 3) * 3 + (j // 3)]:
+                    return False;
+                # else:
+                if board[i][j] != ".":
+                    hm_row[i].add(board[i][j]);
+                if board[j][i] != ".":
+                    hm_col[i].add(board[j][i]);
+                
+                # print(i//3)
+                # print(j//3)
+                if board[i][j] != ".":
+                    hm_box[(i // 3) * 3 + (j // 3)].add(board[i][j]);
+                
+                #check col
+                #check box
+                
+        # print(hm_row)
+        # print(hm_col)
+        print(hm_box)
+        #check column
+        #chck box
+
+        return True;
+
+# [["1","2",".",".","3",".",".",".","."],
+#  ["4",".",".","5",".",".",".",".","."],
+#  [".","9","8",".",".",".",".",".","3"],
+#  ["5",".",".",".","6",".",".",".","4"],
+#  [".",".",".","8",".","3",".",".","5"],
+#  ["7",".",".",".","2",".",".",".","6"],
+#  [".",".",".",".",".",".","2",".","."],
+#  [".",".",".","4","1","9",".",".","8"],
+#  [".",".",".",".","8",".",".","7","9"]]
