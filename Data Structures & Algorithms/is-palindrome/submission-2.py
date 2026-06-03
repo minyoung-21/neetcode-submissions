02e4766159b924f5010b749d564a035b4427f0de
Min_Young Park <minyoungpark5321@gmail.com>
2026-06-03 12:24:20 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        arr_one = [];
+        arr_two = [];
+        for c in s:
+            if c.isalnum():
+                arr_one.append(c.lower())
+                arr_two.insert(0,c.lower())
+        return arr_one == arr_two;
